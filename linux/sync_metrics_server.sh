06ab851d024636970056aaa3d94cac28b71ad2c2
zeus <zeus@zeus.cs.man.ac.uk>
2017-12-15 13:19:23 +0000

    pw less sync

--- linux/sync_metrics_server.sh
+++ linux/sync_metrics_server.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# rsync -avzhe ssh backup.tar root@192.168.0.100:/backups/
 METRICS=/home/zeus/evowl/metrics/
 TARGET=/home/owl/evowl/metrics/
 
-rsync -avXrz --delete -e ssh $METRICS owl@130.88.192.36:$TARGET
+rsync -avHe ssh owl@130.88.192.36:$TARGET $METRICS
--- linux/update_metrics.sh
+++ linux/update_metrics.sh
@@ -1,12 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 METRICS=/home/zeus/evowl/metrics/
+METRICSCOMPLETED=/home/zeus/evowl/metrics_completed/
+
+mkdir -p $METRICSCOMPLETED
 
 for o in `ls -p "$METRICS"`; 
 do
 	rdf=$METRICS$o
-	echo $rdf
+	rdfc=$METRICSCOMPLETED$o
 	WS="http://130.88.193.79:8080/rdf4j-server/repositories/evowl_one/statements?context=%3Chttp://ontothenextone.net/"$o"%3E"
-	echo $WS
-	curl -X PUT -H "Content-type: application/rdf+xml" --data-binary @$rdf $WS
+	
+	echo $rdf
+	
+	
+	if [ ! -f $rdfc ]; then
+		echo "New file, processing..."
+		curl -X PUT -H "Content-type: application/rdf+xml" --data-binary @$rdf $WS
+		cp $rdf $METRICSCOMPLETED
+	else
+		if cmp -s "$rdf" "$rdfc" ; then
+			echo "File has already been processed, ignoring.."
+		else
+			echo "File has changed, processing again.."
+			curl -X PUT -H "Content-type: application/rdf+xml" --data-binary @$rdf $WS
+			cp $rdf $METRICSCOMPLETED
+		fi
+	fi
 done	
